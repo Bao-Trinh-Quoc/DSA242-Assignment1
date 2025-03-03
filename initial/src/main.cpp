473b4c4c82ec1a2de0ac15bd2dd1706722c4a3b0
Bao-Trinh-Quoc <131318295+Bao-Trinh-Quoc@users.noreply.github.com>
2025-03-03 03:58:33 +0000

    feat: update XArrayList code and tested

--- initial/src/main.cpp
+++ initial/src/main.cpp
@@ -2,28 +2,35 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
-#include "test/tc_dlinkedlist.h"
+// #include "test/tc_dlinkedlist.h"
 #include "test/tc_xarraylist.h"
-#include "test/tc_inventory.h"
+// #include "test/tc_inventory.h"
 
 using namespace std;
 
-void (*func_ptr[15])() = {
-    dlistDemo1,
-    dlistDemo2,
-    dlistDemo3,
-    dlistDemo4,
-    dlistDemo5,
-    dlistDemo6,
+// void (*func_ptr[15])() = {
+//     dlistDemo1,
+//     dlistDemo2,
+//     dlistDemo3,
+//     dlistDemo4,
+//     dlistDemo5,
+//     dlistDemo6,
+//     xlistDemo1,
+//     xlistDemo2,
+//     xlistDemo3,
+//     xlistDemo4,
+//     tc_inventory1001,
+//     tc_inventory1002,
+//     tc_inventory1003,
+//     tc_inventory1004,
+//     tc_inventory1005
+// };
+
+void (*func_ptr[4])() = {
     xlistDemo1,
     xlistDemo2,
     xlistDemo3,
-    xlistDemo4,
-    tc_inventory1001,
-    tc_inventory1002,
-    tc_inventory1003,
-    tc_inventory1004,
-    tc_inventory1005
+    xlistDemo4
 };
 
 void run(int func_idx)
@@ -35,7 +42,11 @@ int main(int argc, char **argv)
 {
     // cout << "Assignment-1" << endl;
     if (argc == 1) {
-        tc_inventory1001();
+        // tc_inventory1001();
+        for (int i = 0; i < 4; i++) {
+            cout << "==========Running test case=======: " << i + 1 << endl;
+            run(i);
+        }
     }
     
     return 0;
